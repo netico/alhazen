41feaa0a9f34f2a2d42ba7066ea35982d5d8d434
Luca Bonfanti <lucolo96@gmail.com>
2020-01-09 18:31:47 +0100

    Added middleware to log the requests

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+const httpContext = require('express-http-context');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const config = require('./config');
+const logger = require('./config/logger');
+const { randomString } = require('./lib/utils');
 
 const indexRouter = require('./routes');
 const viewsRouter = require('./routes/views');
@@ -18,6 +21,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(httpContext.middleware);
+
+app.use((req, res, next) => {
+  httpContext.set('reqId', randomString(16));
+  return next();
+});
+
+app.use((req, res, next) => {
+  logger.log('info', 'recived %s - %s', req.method, req.originalUrl);
+  return next();
+});
 
 app.use('/', indexRouter);
 
